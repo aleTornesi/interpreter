e5e92aa66a69232d66c8ea02afd5cef4cf8ef798
Alessandro Tornesi <alessandrotornesi@Alessandros-MacBook-Pro.local>
2025-11-23 11:15:59 +0100

    basic tokens definition

---
+++ bin/tokens.ml
@@ -0,0 +1,15 @@
+type token =
+    Let |
+    Int of int |
+    Identifier of string |
+    Function |
+    Illegal |
+    EOF |
+    Assign |
+    Plus |
+    Comma |
+    Semicolon |
+    LParenthesis |
+    RParanthesis |
+    LBrace |
+    RBrace
